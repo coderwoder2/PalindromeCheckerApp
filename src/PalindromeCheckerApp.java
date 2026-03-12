f08da55c4c9722258685a5475acb2957a2d2a388
KushagraTyagi <kushagratyagi840@gmail.com>
2026-03-12 21:30:11 +0530

    UC7:Deque-Based Optimized Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
 
-        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter input: ");
         String input = sc.nextLine();
@@ -11,27 +10,34 @@ public class PalindromeCheckerApp {
 
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
         char[] queue = new char[n];
+        char[] deque = new char[input.length()];
         int front = 0;
         int rear = 0;
+        int rear = -1;
 
         for (int i = 0; i < n; i++) {
             char ch = input.charAt(i);
             stack.push(ch);
             queue[rear++] = ch;
+        for (char c : input.toCharArray()) {
+            deque[++rear] = c;
         }
 
         boolean isPalindrome = true;
 
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) != stack.pop()) {
         while (!stack.isEmpty()) {
             char fromStack = stack.pop();
             char fromQueue = queue[front++];
 
             if (fromStack != fromQueue) {
+        while (front < rear) {
+            if (deque[front] != deque[rear]) {
                 isPalindrome = false;
                 break;
             }
+            front++;
+            rear--;
+        }
+
+        System.out.println("Input : " + input);
